72cc70eb49b15598731146cc5b38554e8e66e64b
weiqiuke <wqk9409@163.com>
2021-04-06 10:04:43 +0800

    test

--- shangjupay-merchant/shanjupay-merchant-api/src/main/java/com/shanjupay/merchant/api/MerchantService.java
+++ shangjupay-merchant/shanjupay-merchant-api/src/main/java/com/shanjupay/merchant/api/MerchantService.java
@@ -5,7 +5,7 @@ import com.shanjupay.merchant.api.dto.MerchantDTO;
 public interface MerchantService {
 
     /**
-     * 根据ID查询详细信息
+     *  根据ID查询详细信息
      */
     MerchantDTO queryMerchantById(Long merchantId);
 }
