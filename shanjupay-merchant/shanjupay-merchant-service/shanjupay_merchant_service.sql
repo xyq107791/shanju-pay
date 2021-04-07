734b45d7944dba8a3e87b23abd9137ce60e35ed4
weiqiuke <wqk9409@163.com>
2021-04-07 09:42:32 +0800

    sql

---
+++ shanjupay-merchant/shanjupay-merchant-service/shanjupay_merchant_service.sql
@@ -0,0 +1,109 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50728
+ Source Host           : localhost:3306
+ Source Schema         : shanjupay_merchant_service
+
+ Target Server Type    : MySQL
+ Target Server Version : 50728
+ File Encoding         : 65001
+
+ Date: 07/04/2021 09:40:41
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for app
+-- ----------------------------
+DROP TABLE IF EXISTS `app`;
+CREATE TABLE `app`  (
+  `ID` bigint(20) NOT NULL,
+  `APP_ID` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `APP_NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '商店名称',
+  `MERCHANT_ID` bigint(20) DEFAULT NULL COMMENT '所属商户',
+  `PUBLIC_KEY` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '应用公钥(RSAWithSHA256)',
+  `NOTIFY_URL` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '授权回调地址',
+  PRIMARY KEY (`ID`) USING BTREE,
+  UNIQUE INDEX `APP_ID`(`APP_ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of app
+-- ----------------------------
+INSERT INTO `app` VALUES (1379360800460582913, '2bdb0dca50754af29bf2c86f6856be63', '这里输出应用名称', 1379330232364564482, 'string', 'string');
+
+-- ----------------------------
+-- Table structure for merchant
+-- ----------------------------
+DROP TABLE IF EXISTS `merchant`;
+CREATE TABLE `merchant`  (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `MERCHANT_NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '商户名称',
+  `MERCHANT_NO` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '企业编号',
+  `MERCHANT_ADDRESS` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '企业地址',
+  `MERCHANT_TYPE` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '商户类型',
+  `BUSINESS_LICENSES_IMG` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '营业执照（企业证明）',
+  `ID_CARD_FRONT_IMG` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '法人身份证正面照片',
+  `ID_CARD_AFTER_IMG` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '法人身份证反面照片',
+  `USERNAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '联系人姓名',
+  `MOBILE` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '联系人手机号(关联统一账号)',
+  `CONTACTS_ADDRESS` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '联系人地址',
+  `AUDIT_STATUS` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '审核状态 0-未申请,1-已申请待审核,2-审核通过,3-审核拒绝',
+  `TENANT_ID` bigint(20) DEFAULT NULL COMMENT '租户ID,关联统一用户',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1379330232364564483 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of merchant
+-- ----------------------------
+INSERT INTO `merchant` VALUES (1379330232364564482, '学生餐厅', '32321321312', '郑州梧桐创业大厦', '餐饮', '6272d44a‐19e4‐44a7‐a714‐58ffc7da8e45e.png', '6272d44a‐19e4‐44a7‐a714‐58ffc7da8e45e.png', '6272d44a‐19e4‐44a7‐a714‐58ffc7da8e45e.png', '张先生', '15678870532', '郑州梧桐街', '2', NULL);
+
+-- ----------------------------
+-- Table structure for staff
+-- ----------------------------
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE `staff`  (
+  `ID` bigint(20) NOT NULL COMMENT '主键',
+  `MERCHANT_ID` bigint(20) DEFAULT NULL COMMENT '商户ID',
+  `FULL_NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
+  `POSITION` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位',
+  `USERNAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户名(关联统一用户)',
+  `MOBILE` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '手机号(关联统一用户)',
+  `STORE_ID` bigint(20) DEFAULT NULL COMMENT '员工所属门店',
+  `LAST_LOGIN_TIME` datetime(0) DEFAULT NULL COMMENT '最后一次登录时间',
+  `STAFF_STATUS` bit(1) DEFAULT NULL COMMENT '0表示禁用，1表示启用',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for store
+-- ----------------------------
+DROP TABLE IF EXISTS `store`;
+CREATE TABLE `store`  (
+  `ID` bigint(20) NOT NULL,
+  `STORE_NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '门店名称',
+  `STORE_NUMBER` bigint(20) DEFAULT NULL COMMENT '门店编号',
+  `MERCHANT_ID` bigint(20) DEFAULT NULL COMMENT '所属商户',
+  `PARENT_ID` bigint(20) DEFAULT NULL COMMENT '父门店',
+  `STORE_STATUS` bit(1) DEFAULT NULL COMMENT '0表示禁用，1表示启用',
+  `STORE_ADDRESS` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '门店地址',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for store_staff
+-- ----------------------------
+DROP TABLE IF EXISTS `store_staff`;
+CREATE TABLE `store_staff`  (
+  `ID` bigint(20) NOT NULL,
+  `STORE_ID` bigint(20) DEFAULT NULL COMMENT '门店标识',
+  `STAFF_ID` bigint(20) DEFAULT NULL COMMENT '员工标识',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
